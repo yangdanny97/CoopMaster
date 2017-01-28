c7c2b165a98d2c353d6a67208ed98f833126d46f
ltang500 <ltang500@gmail.com>
2017-01-28 16:08:36 -0500

    extract data from file

--- app/src/main/java/com/example/danny/project1/Main2Activity.java
+++ app/src/main/java/com/example/danny/project1/Main2Activity.java
@@ -3,29 +3,53 @@ package com.example.danny.project1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
+import android.util.Log;
+
 import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.helper.DateAsXAxisLabelFormatter;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 
 public class Main2Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         Intent intent = getIntent();
 
-        GraphView graph = (GraphView) findViewById(R.id.graph);
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
-                new DataPoint(0, 1),
-                new DataPoint(1, 5),
-                new DataPoint(2, 3),
-                new DataPoint(3, 2),
-                new DataPoint(4, 6)
-        });
-        graph.addSeries(series);
+        File data = getFilesDir();
+        ArrayList<Integer> date = new ArrayList<Integer>();
+        ArrayList<Integer> eggs = new ArrayList<Integer>();
+        try {
+            BufferedReader bf = new BufferedReader(new FileReader(data));
+            String str;
+            while((str = bf.readLine()) != null) {
+                String[] arg = str.split(",");
+                date.add(Integer.parseInt(arg[0]));
+                eggs.add(Integer.parseInt(arg[1]));
+            }
+        } catch (IOException e) {
+        }
 
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        DataPoint[] d = new DataPoint[date.size()];
+        for(int i = 0;i < date.size();i++) {
+            d[i] = new DataPoint(date.get(i), eggs.get(i));
+        }
+
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(d);
+        graph.addSeries(series);
     }
 }
--- app/src/main/java/com/example/danny/project1/MainActivity.java
+++ app/src/main/java/com/example/danny/project1/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.danny.project1;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
 import android.widget.EditText;
+
+import java.io.File;
+import java.io.FileOutputStream;
 
 public class MainActivity extends AppCompatActivity {
     public final static String DATE = "com.example.project1.MESSAGE";
