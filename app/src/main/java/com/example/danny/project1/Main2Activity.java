b8822df97c88387588280f10e65a50bdcddf7825
Danny Yang <dzy4@cornell.edu>
2017-01-28 12:04:40 -0500

    initial upload

---
+++ app/src/main/java/com/example/danny/project1/Main2Activity.java
@@ -0,0 +1,31 @@
+package com.example.danny.project1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Intent intent = getIntent();
+
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        graph.addSeries(series);
+
+    }
+}
---
+++ app/src/main/java/com/example/danny/project1/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.danny.project1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    public final static String DATE = "com.example.project1.MESSAGE";
+    public final static String EGG = "com.example.project1.MESSAGE";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void submitData(View view) {
+
+        EditText date = (EditText) findViewById(R.id.editText1);
+        EditText eggs = (EditText) findViewById(R.id.editText2);
+        String enteredDate = date.getText().toString();
+        String enteredEggs = eggs.getText().toString();
+        /*intent.putExtra(DATE, enteredDate);
+        intent.putExtra(EGG, enteredEggs);*/
+
+    }
+
+    public void viewGraph (View view){
+        Intent intent = new Intent(this, Main2Activity.class);
+        startActivity(intent);
+    }
+}
